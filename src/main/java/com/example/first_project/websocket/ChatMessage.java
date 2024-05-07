7377bb0135e18f5f7d4bfea914d4b168d5ca531e
hanseongeon <gkstjddjs0517@gamli.com>
2024-05-07 09:15:20 +0900

    Websocket start

--- src/main/java/com/example/first_project/websocket/ChatMessage.java
+++ src/main/java/com/example/first_project/websocket/ChatMessage.java
@@ -1,17 +1,22 @@
 package com.example.first_project.websocket;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Entity
 public class ChatMessage {
     public enum MessageType{
         ENTER,TALK
     }
 
-    private MessageType type; // 메세지 타입
     private String roomId; // 채팅방 번호
     private String sender; // 메세지 보낸사람
     private String message; // 메시지
+
+    @ManyToOne
+    private ChatRoom chatRoom;
 }
--- src/main/java/com/example/first_project/websocket/ChatRoom.java
+++ src/main/java/com/example/first_project/websocket/ChatRoom.java
@@ -1,20 +1,33 @@
 package com.example.first_project.websocket;
 
+import com.example.first_project.user.SiteUser;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
+@Entity
 public class ChatRoom {
     private String roomId;
-    private String name;
 
-    public static ChatRoom create(String name) {
-        ChatRoom chatRoom = new ChatRoom();
-        chatRoom.roomId = UUID.randomUUID().toString();
-        chatRoom.name = name;
-        return chatRoom;
+    @OneToMany
+    List<ChatMessage> chatMessageList = new ArrayList<>();
+
+    @OneToMany
+    List<SiteUser> siteUserList = new ArrayList<>();
+    @Builder
+    public ChatRoom(ChatMessage chatMessage1,ChatMessage chatMessage2,SiteUser user1, SiteUser user2) {
+        this.chatMessageList.add(chatMessage1);
+        this.chatMessageList.add(chatMessage2);
+        this.siteUserList.add(user1);
+        this.siteUserList.add(user2);
+        this.setRoomId(UUID.randomUUID().toString());
     }
 }
