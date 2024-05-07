00ccee65531fe5de04f4bd89bff3c4ed67fb6b81
hanseongeon <gkstjddjs0517@gamli.com>
2024-05-07 15:48:22 +0900

    websockt stomp apply

--- src/main/java/com/example/first_project/websocket/ChatMessage.java
+++ src/main/java/com/example/first_project/websocket/ChatMessage.java
@@ -1,22 +1,33 @@
 package com.example.first_project.websocket;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChatMessage {
     public enum MessageType{
         ENTER,TALK
     }
-
+    @Id
     private String roomId; // 채팅방 번호
+
     private String sender; // 메세지 보낸사람
+
     private String message; // 메시지
+
+
 
     @ManyToOne
     private ChatRoom chatRoom;
+
+    public ChatMessage(String message){this.message = message;}
 }
--- src/main/java/com/example/first_project/websocket/ChatRoomRepository.java
+++ src/main/java/com/example/first_project/websocket/ChatRoomRepository.java
@@ -1,35 +1,9 @@
 package com.example.first_project.websocket;
 
-import com.example.first_project.user.SiteUser;
-import jakarta.annotation.PostConstruct;
-import org.springframework.stereotype.Repository;
 
-import java.util.*;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-
-// import 생략....
-
-@Repository
-public class ChatRoomRepository {
-
-    private Map<String, ChatRoom> chatRoomMap;
-
-    @PostConstruct
-    private void init() {
-        chatRoomMap = new LinkedHashMap<>();
-    }
-
-
-    public List<ChatRoom> findAllRoom() {
-        // 채팅방 생성순서 최근 순으로 반환
-        List<ChatRoom> chatRooms = new ArrayList<>(chatRoomMap.values());
-        Collections.reverse(chatRooms);
-        return chatRooms;
-    }
-
-    public ChatRoom findRoomById(String id) {
-        return chatRoomMap.get(id);
-    }
+public interface ChatRoomRepository extends JpaRepository<ChatRoom,String> {
 
 
 }
